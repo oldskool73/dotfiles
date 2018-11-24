15204f3ef625f81556ad492543fd1b08bbbfded1
Adam Robertson <adam.robertson@imagination.com>
2018-11-24 17:29:27 +1100

    try to fix ruby install, seems broken still

--- install/gem.sh
+++ install/gem.sh
@@ -5,11 +5,17 @@ fi
 
 brew install gpg2
 
+# key import failing!
+#gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+
 \curl -sSL https://get.rvm.io | bash -s stable
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+source ~/.rvm/scripts/rvm
 
-rvm install 2.5
-rvm use 2.5 --default
+~/.rvm/bin/rvm install 2.5
 
+# ...this doesn't work until you launch a new terminal...
+
+~/.rvm/bin/rvm use 2.5 --default
 gem install pygmentize
