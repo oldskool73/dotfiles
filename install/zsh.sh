72378051662cd0075c0eac5b6e27c29d35cf6dbb
Adam Robertson <adam.robertson@imagination.com>
2018-11-24 16:11:10 +1100

    fix zshrc and install powerline & fonts

--- install/zsh.sh
+++ install/zsh.sh
@@ -6,3 +6,13 @@ fi
 brew install zsh
 brew install antigen
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# relink zshrc as oh-my overwrites it
+ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
+
+# install powerline & fonts
+pip3 install powerline-status
+brew cask install font-powerline-symbols \
+  font-source-code-pro-for-powerline \
+  font-menlo-for-powerline \
+  font-roboto-mono-for-powerline
